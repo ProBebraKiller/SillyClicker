f1b1cd0e05112f1c89e37f0e62a3c6da095352b5
userNotAvalible <foxy6868foxy@gmail.com>
2024-03-02 14:50:30 +0100

    array repeater

--- Silly Cars Clicker/Assets/Scripts/Passive Script.cs
+++ Silly Cars Clicker/Assets/Scripts/Passive Script.cs
@@ -6,19 +6,8 @@ public class PassiveScript : MonoBehaviour
 {
     void Start()
     {
-        InvokeRepeating("passiveEarning", 1f, 1f);
-    }
-
-    void passiveEarning()
-    {
-        ClickingScript.cookies += BuyScript.CatWorkerAmount;
-
-
-        ClickingScript.CookiesText.text = ClickingScript.cookies.ToString();
-    }
-
-    void Update()
-    {
 
     }
+
+
 }
